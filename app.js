76403aa9f967c5ed084e46cbc98c54d5d7125f37
Olamide <olasupoolamide00@gmail.com>
2023-09-07 20:26:01 +0100

    Zuri First Task

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 const date = new Date();
 
-const days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 const today = days[date.getDay()];
 
-
-
 const updateTime = () => {
   const date = new Date();
-  const utcTime = date.toISOString().substr(11,8);
+  const utcTime = date.toISOString().substr(11, 8);
   timeUtc.textContent = utcTime;
-}
+};
 
 const todayTag = document.getElementById('today');
 const timeUtc = document.getElementById('utc');
 
 todayTag.textContent = today;
 
-setInterval(updateTime,1000);
+setInterval(updateTime, 1000);
